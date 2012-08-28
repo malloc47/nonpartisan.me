57ba19081dc78d3cecfab87151e85f7affe33d7b
Jarrell Waggoner <malloc47@gmail.com>
2012-08-28 09:40:03 -0400

    updated method that is an order of magnitude faster

--- nonpartisan.js
+++ nonpartisan.js
@@ -1,18 +1,18 @@
-function findContent(classname,container,removeList){
-    // $('li.genericStreamStory').each(function() {
-    // 	if($(this).find('*')
-    // });
-    $(classname).filter( function (index) {
-	var toCheck = $(this).text().toLowerCase();
-	if(toCheck.length == 0) {return false;}
-	return removeList
-	    .some(function(value) {
-		// if(toCheck.indexOf(value.toLowerCase()) >= 0) {
-		//     console.debug(toCheck);
-		// }
-		return (toCheck.indexOf(value.toLowerCase()) >= 0);
-	    });
-    }).closest(container).css({'display':'none'});
+function findContent(searchName,removeList){
+    $(searchName).each(function() {
+	var el = $(this);
+    	el.find('*').each(function () {
+	    var toCheck = $(this).text().toLowerCase();
+	    if(toCheck.length > 0 &&
+	       (removeList.some(function(value) {
+		   return (toCheck.indexOf(value.toLowerCase()) >= 0);
+	       }))
+	      ) {
+		el.css({'display':'none'});
+		return false;
+	    }
+	});
+    });
 };
 
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
@@ -20,7 +20,7 @@ var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
 	if(mutation.type == 'childList') {
 	    console.debug("removing from mutation");
-	    findContent('li.genericStreamStory *','li.genericStreamStory',['the','project']);
+	    findContent('li.genericStreamStory',['the','project']);
 	}
     });
 });
@@ -32,4 +32,4 @@ observer.observe(document.querySelector('#mainContainer'),
 		   , subtree       : true });
 
 // findContent('li.genericStreamStory *:not(:has(*))','li.genericStreamStory',['the','project']);
-findContent('li.genericStreamStory *','li.genericStreamStory',['the','project']);
+findContent('li.genericStreamStory',['the','project']);

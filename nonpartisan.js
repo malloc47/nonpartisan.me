0b1dc645d8330184a3beff636b8ef53e9f7e7783
Jarrell Waggoner <malloc47@gmail.com>
2012-08-28 22:29:34 -0400

    fully working, but with leaking MutationObserver

--- nonpartisan.js
+++ nonpartisan.js
@@ -1,35 +1,61 @@
-function findContent(searchName,removeList){
-    $(searchName).each(function() {
-	var el = $(this);
-    	el.find('*').each(function () {
-	    var toCheck = $(this).text().toLowerCase();
-	    if(toCheck.length > 0 &&
-	       (removeList.some(function(value) {
-		   return (toCheck.indexOf(value.toLowerCase()) >= 0);
-	       }))
-	      ) {
-		el.css({'display':'none'});
-		return false;
+function nonpartisan(watch,parent,keywords) {
+
+    function kill(parent,removeList){
+	$(parent).each(function() {
+	    var el = $(this);
+	    if(el.css('display') != 'none') {
+    		el.find('*').each(function () {
+		    var toCheck = $(this).text().toLowerCase();
+		    if(toCheck.length > 0 &&
+		       (removeList.some(function(value) {
+			   return (toCheck.indexOf(value.toLowerCase()) >= 0);
+		       }))
+		      ) {
+			el.css({'display':'none'});
+			return false;
+		    }
+		});
 	    }
 	});
-    });
-};
+    };
 
-var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-	if(mutation.type == 'childList') {
-	    console.debug("removing from mutation");
-	    findContent('li.genericStreamStory',['the','project']);
-	}
-    });
-});
+    // if on a page with the supported elements
+    if($(parent) && $(watch)) {
 
-observer.observe(document.querySelector('#mainContainer'), 
-		 { attributes      : false
-		   , childList     : true
-		   , characterData : false
-		   , subtree       : true });
+	var numChildren = $(parent).children().length
+	
+	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+	var observer = new MutationObserver(function(mutations) {
+	    if(mutations
+	       .some(function(el,ind,a) {
+		   return (el.type == 'childList');
+	       })) {
+	    	kill(parent,keywords);
+	    }
+	});
 
-// findContent('li.genericStreamStory *:not(:has(*))','li.genericStreamStory',['the','project']);
-findContent('li.genericStreamStory',['the','project']);
+	observer.observe(document.querySelector(watch), 
+			 { attributes      : false
+			   , childList     : true
+			   , characterData : false
+			   , subtree       : true });
+
+	kill(parent,keywords);
+    }
+}
+
+function fb_nonpartisan(keywords) {
+    // newsfeed
+    nonpartisan('#content','li.genericStreamStory',keywords);
+    // newsticker
+    nonpartisan('#content','div.fbFeedTickerStory',keywords);
+    // timeline
+    nonpartisan('#content','li.fbTimelineUnit',keywords);
+}
+
+democrat = ['obama','health care','abortion','pro-choice','liberal'];
+republican = ['mitt','romney','paul ryan','pro-life','conservative','gop','rnc','ron paul'];
+topics = ['abortion','election','government','contraception','taxes'];
+all = topics.concat(democrat,republican);
+
+fb_nonpartisan(all);

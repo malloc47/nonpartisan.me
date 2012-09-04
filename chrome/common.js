3c78b86d5d1d4b2ffa45e7d9aa258af0dce0f5ac
Jarrell Waggoner <malloc47@gmail.com>
2012-09-03 22:04:56 -0400

    new keywords and version bump

--- chrome/common.js
+++ chrome/common.js
@@ -1,6 +1,5 @@
 var choices = {
-    "conservative" : ['mitt','romney','paul ryan','pro-life','conservative','gop','rnc','ron paul'],
-    "liberal"      : ['obama','health care','pro-choice','liberal','dnc'],
-    "topics"       : ['abortion','election','government','contraception','taxes','voter','political',
-		      'politics'],
+    "conservative" : ['conservative','conservatives','republican','republicans','mitt','romney','paul ryan','pro-life','gop','rnc','ron paul'],
+    "liberal"      : ['liberal','liberals','democrat','democrats','barak','obama','health care','pro-choice','dnc'],
+    "topics"       : ['abortion','election','government','contraception','taxes','voter','political','politics'],
 };

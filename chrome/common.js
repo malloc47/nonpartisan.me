b3312b7dcd9dd126039b0ee97621aa161e2b568f
Jarrell Waggoner <malloc47@gmail.com>
2012-09-06 22:22:08 -0400

    well, that was embarrassing

--- chrome/common.js
+++ chrome/common.js
@@ -1,5 +1,5 @@
 var choices = {
     "conservative" : ['conservative','conservatives','republican','republicans','mitt','romney','paul ryan','pro-life','gop','rnc','ron paul'],
-    "liberal"      : ['liberal','liberals','democrat','democrats','barak','obama','health care','pro-choice','dnc'],
+    "liberal"      : ['liberal','liberals','democrat','democrats','barack','obama','health care','pro-choice','dnc','joe biden'],
     "topics"       : ['abortion','election','government','contraception','taxes','voter','political','politics'],
 };

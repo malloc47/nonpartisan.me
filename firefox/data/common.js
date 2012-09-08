566f3f65e81e9136c90b216c5db9b25716a57f2e
Jarrell Waggoner <malloc47@gmail.com>
2012-09-07 23:51:30 -0400

    (firefox) more keywords and a version bump

--- firefox/data/common.js
+++ firefox/data/common.js
@@ -1,5 +1,5 @@
 var choices = {
-    "conservative" : ['conservative','conservatives','republican','republicans','mitt','romney','paul ryan','pro-life','gop','rnc','ron paul'],
-    "liberal"      : ['liberal','liberals','democrat','democrats','barack','obama','health care','pro-choice','dnc','joe biden'],
-    "topics"       : ['abortion','election','government','contraception','taxes','voter','political','politics'],
+    "conservative" : ['conservative','conservatives','republican','republicans','mitt','romney','paul ryan','pro-life','gop','rnc','ron paul','eastwood','bain'],
+    "liberal"      : ['liberal','liberals','democrat','democrats','barack','obama','health care','pro-choice','dnc','joe biden','obamacare'],
+    "topics"       : ['abortion','election','elections','government','contraception','taxes','voter','voters','political','politics','president','presidents','socialism'],
 };
